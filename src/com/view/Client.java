de180b798f094cb7e08f49c953a83de3001dee5a
robinaiden <a10423041@yuntech.org.tw>
2016-10-21 01:34:55 +0800

    JTable成功放入資料並顯示

--- src/com/view/Client.java
+++ src/com/view/Client.java
@@ -1,71 +1,30 @@
 package com.view;
 
-import java.awt.EventQueue;
 import java.awt.Image;
 
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
+import java.util.Random;
 import java.awt.event.ActionEvent;
 import java.awt.Font;
 import javax.swing.JTable;
+import java.awt.BorderLayout;
 
 public class Client {
 	
-	private static JButton btnGrouping;
+	private JButton btnGrouping;
 	private JFrame frame;
 	private JTable table;
+	private JScrollPane scrollPane;
 
 	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					Client window = new Client();
-					window.frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-		
-		btnGrouping.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				JOptionPane.showMessageDialog(null, "你好~");
-			}
-		});
-		
-		/* 學生欄位的數值 */
-		String [] header= {"學號","英文能力","程式能力"};
-		String [][] data = {{"A10423001", "60", "20"},
-							{"A10423002", "70", "30"},
-							{"A10423003", "20", "80"},
-							{"A10423004", "20", "80"},
-							{"A10423005", "20", "80"},
-							{"A10423006", "20", "80"},
-							{"A10423007", "20", "80"},
-							{"A10423008", "20", "80"},
-							{"A10423009", "20", "80"},
-							{"A10423010", "20", "80"},
-							{"A10423011", "20", "80"},
-							{"A10423012", "20", "80"},
-							{"A10423013", "20", "80"},
-							{"A10423014", "20", "80"},
-							{"A10423015", "20", "80"},
-							{"A10423016", "20", "80"},
-							{"A10423017", "20", "80"},
-							{"A10423018", "20", "80"},
-							{"A10423019", "20", "80"},
-							{"A10423020", "20", "80"},
-							{"A10423021", "20", "80"},
-							{"A10423022", "20", "80"},
-							{"A10423023", "20", "80"},
-							{"A10423024", "20", "80"},
-							{"A10423025", "20", "80"}};
-		
-		
-		
+		Client window = new Client();
+		window.frame.setVisible(true);
 	}
 
 	/**
@@ -80,25 +39,60 @@ public class Client {
 	 */
 	private void initialize() {
 		
-		frame = new JFrame();
+		frame = new JFrame("OOSE Grouping");
 		frame.getContentPane().setFont(new Font("Consolas", Font.PLAIN, 16));
 		frame.setBounds(100, 100, 800, 500);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(null);
+//		frame.getContentPane().setLayout(null);
 		
 		/* 分組按鈕 */
-		JButton btnGrouping = new JButton("Grouping");
+		btnGrouping = new JButton("Grouping");
 		btnGrouping.setFont(new Font("Consolas", Font.PLAIN, 16));
 		Image img = new ImageIcon(this.getClass().getResource("/icon_old_go_next.png")).getImage();
+		frame.getContentPane().setLayout(null);
 		btnGrouping.setIcon(new ImageIcon(img));
-		btnGrouping.setBounds(319, 373, 139, 27);
+		btnGrouping.setBounds(287, 399, 246, 41);
 		frame.getContentPane().add(btnGrouping);
 		
-		/* 學生欄位 */
-		table = new JTable();
-		table.setBounds(421, 102, 324, 210);
-		frame.getContentPane().add(table);
 		
+		table = new JTable(getStudentData(), getTableName());
+		scrollPane = new JScrollPane(table);
+		scrollPane.setBounds(14, 13, 246, 226);
+		table.setBounds(421, 102, 324, 210);
+		frame.getContentPane().add(scrollPane);
+		
+		frame.getContentPane().setVisible(true);
+		
+		btnGrouping.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				JOptionPane.showMessageDialog(null, "你好~");
+			}
+		});
+		
+	}
+	
+//	private void createTable() {
+//		table = new JTable(getStudentData(), getTableName());
+//		table.setBounds(421, 102, 324, 210);
+//		JScrollPane js = new JScrollPane(table);
+//		frame.getContentPane().add(js);
+//		frame.getContentPane().setVisible(true);
+//	}
+	
+	private String[] getTableName() {
+		/* 學生欄位 */
+		String [] columns= {"學號","英文能力","程式能力"};
+		return columns;
+	}
+	
+	private Object[][] getStudentData() {
+		/* 產生隨機值 */
+		Random rand = new Random();
+		/* 學生欄位的數值 */
+		Object [][] data = {{"A10423001", rand.nextInt(100), rand.nextInt(100)},
+							{"A10423002", rand.nextInt(100), rand.nextInt(100)},
+							{"A10423003", rand.nextInt(100), rand.nextInt(100)}};
+		return data;
 	}
 	
 }
